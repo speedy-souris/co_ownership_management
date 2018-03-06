4a2fdbad825131001105b5e3b0068a161f5b87cd
pascal <pascal.macchi@gmail.com>
2018-03-06 15:04:22 +0100

    ERREUR

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ class Co_owner:
     # define a co-owner
     NUMBER_CO_OWNER = 1
 
-    def __init__(self, name, quota_share1, quota_share2):
+    def __init__(self, name, quota_share1=1371, quota_share2=712):
         
-        print("Copropriaitre Numéro ", Co_owner.NUMBER_CO_OWNER)
+        print("Coproprietaire N° ", Co_owner.NUMBER_CO_OWNER)
+        
         self.name = name
         self.quota_share1 = quota_share1
         self.quota_share2 = quota_share2
@@ -13,8 +14,10 @@ class Co_owner:
 
 
 # initialization of co-owner
-name_owner = input("Donnez un nom au coproprietaire ")
-owner_quota = int(input("quote-part principale "))
-owner_quota2 = int(input("quote_part secondaire "))
-owner = Co_owner(name_owner, owner_quota, owner_quota2 )
-print("{} a une quote-part principale de {} et une secondaire de {}".format(owner.name, owner.quota_share1, owner.quota_share2))
+while Co_owner.NUMBER_CO_OWNER < 8:
+    
+    name_owner = input("Donnez un nom au coproprietaire ")        
+    
+    owner = Co_owner(name_owner)
+
+    print("{} a une quote-part principale de {} et une secondaire de {}".format(owner.name, owner.quota_share1, owner.quota_share2))
